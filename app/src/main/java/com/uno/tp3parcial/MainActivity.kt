59fcec275e39bb626b20253b63b60294c1dd7f97
Tadeo <conversotadeo@gmail.com>
2024-05-30 01:40:27 -0300

    MainActivity ok | UI inicial ok | WIP: Intro Binding, hay fallos en el inflater

--- app/src/main/java/com/uno/tp3parcial/MainActivity.kt
+++ app/src/main/java/com/uno/tp3parcial/MainActivity.kt
@@ -1,20 +1,76 @@
 package com.uno.tp3parcial
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.uno.tp3parcial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    private val fragmentsNavigation = setOf(
+        R.id.navigation_favorites,
+    )
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.contentMainInclude.customToolbar)
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        val navView: BottomNavigationView = binding.navView
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = fragmentsNavigation,
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
+        )
+
+        binding.contentMainInclude.customToolbar.setupWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+        binding.navigationView.setupWithNavController(navController)
+
+
+        navController.addOnDestinationChangedListener { _, destination: NavDestination, _ ->
+            //Esto es para configurar el Icono Fijo
+            //supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_bar_chart_black_24dp)
+
+            when (destination.id) {
+                R.id.navigation_favorites,
+                -> {
+                    navView.visibility = BottomNavigationView.VISIBLE
+                    binding.contentMainInclude.customToolbar.visibility = MaterialToolbar.VISIBLE
+                }
+                else -> {
+                    navView.visibility = BottomNavigationView.GONE
+                    binding.contentMainInclude.customToolbar.visibility = View.GONE
+                }
+            }
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        //Fuezo al boton de navegación de la toolbar que solo abra el menú Drawer
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/uno/tp3parcial/ui/splashScreen/SplashScreen.kt
@@ -0,0 +1,11 @@
+package com.uno.tp3parcial.ui.splashScreen
+
+import androidx.fragment.app.Fragment
+
+
+class SplashScreen : Fragment() {
+
+
+    private var _binding: FragmentIntroBinding? = null
+
+}
