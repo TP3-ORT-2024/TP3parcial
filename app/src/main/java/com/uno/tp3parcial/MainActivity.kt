d76c274a9e8a87a7a507e85ea038b935ea1ae7ad
Tadeo <conversotadeo@gmail.com>
2024-06-01 18:54:15 -0300

    done: bottom navbar

--- app/src/main/java/com/uno/tp3parcial/MainActivity.kt
+++ app/src/main/java/com/uno/tp3parcial/MainActivity.kt
@@ -1,76 +1,25 @@
 package com.uno.tp3parcial
 
-import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.uno.tp3parcial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var navController: NavController
-
-    private val fragmentsNavigation = setOf(
-        R.id.navigation_favorites,
-    )
-
-    private lateinit var binding: ActivityMainBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        setSupportActionBar(binding.contentMainInclude.customToolbar)
-        navController = findNavController(R.id.nav_host_fragment_activity_main)
-
-        val navView: BottomNavigationView = binding.navView
-        val appBarConfiguration = AppBarConfiguration(
-            topLevelDestinationIds = fragmentsNavigation,
-            fallbackOnNavigateUpListener = ::onSupportNavigateUp
-        )
-
-        binding.contentMainInclude.customToolbar.setupWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
-        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
-        binding.navigationView.setupWithNavController(navController)
-
-
-        navController.addOnDestinationChangedListener { _, destination: NavDestination, _ ->
-            //Esto es para configurar el Icono Fijo
-            //supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_bar_chart_black_24dp)
-
-            when (destination.id) {
-                R.id.navigation_favorites,
-                -> {
-                    navView.visibility = BottomNavigationView.VISIBLE
-                    binding.contentMainInclude.customToolbar.visibility = MaterialToolbar.VISIBLE
-                }
-                else -> {
-                    navView.visibility = BottomNavigationView.GONE
-                    binding.contentMainInclude.customToolbar.visibility = View.GONE
-                }
-            }
-        }
+        setupBottomNavBar()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        //Fuezo al boton de navegación de la toolbar que solo abra el menú Drawer
-        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            binding.drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            binding.drawerLayout.openDrawer(GravityCompat.START)
-        }
-        return true;
+    private fun setupBottomNavBar() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        findViewById<BottomNavigationView>(R.id.bottom_bar).setupWithNavController(navController)
     }
 }
