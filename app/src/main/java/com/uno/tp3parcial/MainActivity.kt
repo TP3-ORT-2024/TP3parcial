0c1e8c0090588bcd90d9e1173f7a65de7c168551
Tomas Bou <boutomasezequiel@gmail.com>
2024-06-03 17:26:57 -0300

    Navegacion de profile a settings

--- app/src/main/java/com/uno/tp3parcial/MainActivity.kt
+++ app/src/main/java/com/uno/tp3parcial/MainActivity.kt
@@ -2,11 +2,14 @@ package com.uno.tp3parcial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
